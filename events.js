8dfd0e80d9dd3df65a3499b5a4760e688749892c
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-01-18 15:22:21 +1300

    green div

--- events.js
+++ events.js
@@ -1,13 +1,14 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
-  one()
-    
+  one();
+  two();
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -21,8 +22,19 @@ function one () {
   // Finally, we add one to make the colour white again
   one.addEventListener('mouseleave', makeWhite)
 }
-
 // CREATE FUNCTION two HERE
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we add an event listener to it:
+  two.addEventListener('mouseenter', makeGreen)
+
+  // Finally, we add one to make the colour white again
+  two.addEventListener('mouseleave', makeWhite)
+}
+
+
 
 // CREATE FUNCTION three HERE
 
@@ -35,4 +47,8 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
 }
