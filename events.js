bfcb110504db9ea5c6ca5bcc438f3bcf3bfb3adf
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-01-18 15:54:16 +1300

    click event

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -45,8 +46,15 @@ function three () {
   // Finally, we add one to make the colour white again
   three.addEventListener('mouseleave', makeWhite)
 }
-
 // CREATE FUNCTION four HERE
+function four () {
+  // First, we have to find the element:
+  var four = document.getElementById('four')
+
+  // Next, we add an event listener to it:
+  four.addEventListener('click', makeGrey)
+
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -62,4 +70,7 @@ function makeGreen (evt) {
 }
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange'
+}
+function makeGrey (evt) {
+  evt.target.style.backgroundColor = 'grey'
 }
